ff14fab8d0973862a4161f5c1497d4ed821a2806
Victor Lima <victorl1ma@hotmail.com>
2023-01-15 10:17:51 -0300

    responsiveness

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -171,26 +171,30 @@ export default function Home() {
         <div className="howHelpTitle-area">
           <h1 className="howHelp-subTitle">INFORMAÇÕES</h1>
           <h1 className="howHelp-title">COMO O MOTION TIME</h1>
-          <h1 className="howHelp-title">PODE ME AJUDAR</h1>
+          <h1 className="howHelp-title">PODE TE AJUDAR</h1>
         </div>
         <div className="howHelp-item">
           <div className="howHelp-circle">
             <svg
               xmlns="http://www.w3.org/2000/svg"
+              width={184}
+              height={184}
               x="0px"
               y="0px"
               viewBox="0 0 1000 1000"
+              fill="#293629"
             >
               <path
                 d="M462.9 1189.8l-262 262c-4.1 4.4-6.2 9.8-6.2 16s2.1 11.5 6.2 16l103.9 103.5c4.1 4.4 9.4 6.7 15.8 6.7s11.8-2.2 16.2-6.7l126.1-126.1c4.4-4.4 9.8-6.7 16-6.7 6.2 0 11.5 2.2 16 6.7l352.1 352c4.4 4.4 9.8 6.7 16 6.7 6.2 0 11.5-2.2 16-6.7l104.4-102.6c4.4-4.4 6.7-9.8 6.7-16 0-6.2-2.2-11.5-6.7-16L495 1189.7c-4.4-4.1-9.8-6.2-16-6.2-6.3 0-11.6 2.1-16.1 6.3zm-275.3-169.7c-32 0-61.6 7.9-89 23.8s-49 37.4-64.8 64.6-23.8 57-23.8 89.3v444c0 32 7.9 61.6 23.8 89 15.8 27.4 37.4 49 64.8 64.8s57.1 23.8 89 23.8h444c20.4 0 41-3.8 61.7-11.5l-77.3-77.3H143.2c-8 0-15.4-2-22.2-6-6.8-4-12.2-9.4-16.2-16.2-4-6.8-6-14.2-6-22.2v-532.8c0-12.1 4.4-22.6 13.1-31.3s19.2-13.1 31.3-13.1H676c12.1 0 22.6 4.4 31.3 13.1s13.1 19.2 13.1 31.3v119.4l88.8 88.8v-163.9c0-32.3-7.9-62-23.8-89.3-15.8-27.2-37.4-48.8-64.8-64.6-27.4-15.8-57.1-23.8-89-23.8h-444z"
                 transform="matrix(1 0 0 -1 0 1920)"
               />
             </svg>
-            <p className="howHelp-text">
-              Você vai conseguir editar seus vídeos do jeito mais fácil, rápido
-              e mais atraente.
-            </p>
           </div>
+          <p className="howHelp-text">
+            Você vai conseguir editar seus <br /> vídeos do jeito mais fácil,
+            <br />
+            rápido e mais atraente.
+          </p>
         </div>
       </div>
     </div>
